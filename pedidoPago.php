da755615f9de7ef1d798600e0dd0e64dd4f131b2
tomi tenaglia <tomastenaglia27@gmail.com>
2019-09-23 14:13:00 -0300

    proceso pago pedidos

--- pedidoPago.php
+++ pedidoPago.php
@@ -30,7 +30,7 @@ $idComprobante = $_REQUEST['idComprobante'];
       <div class="card text-center">
         <h3 class="card-header primary-color white-text">Forma de pago</h3>
         <div class="card-body">
-        <form method="post">
+        <form name="pago" id="pago" method="post">
           <div class="card">
             <div class="card-header bg-light">Datos del Pedido</div>
               <div class="card-body">
@@ -43,22 +43,22 @@ $idComprobante = $_REQUEST['idComprobante'];
                   </div>
                   <div class="col-md-4 mb-4">
                     <div class="md-form">
-                      <input type="number" id="form3" class="form-control" name="importe" value="<?php echo $importe; ?>" disabled>
+                      <input  class="form-control"  type="number" id="importe" name="importe"  value="<?php echo $importe; ?>"  disabled>
                       <label for="form3" class="">Importe</label>
                     </div>
                   </div>
                   <div class="col-md-4 mb-4">
                     <div class="md-form">
-                      <input type="number" id="form3" class="form-control" name="efectivo"   required>
-                      <label for="form3" class="">Efectivo</label>
+                      <input  class="form-control" type="number" id="efectivo" name="efectivo" oninput="calcularCheque(this);" min="0" required>
+                      <label>Pago en efectivo $</label>
                     </div>
                   </div>
                 </div>
                 <div class="row">
                   <div class="col-md-4 mb-4">
                     <div class="md-form">
-                      <input type="number" id="form3" class="form-control" name="totalcheque"   disabled>
-                      <label for="form3" class="">Importe a pagar con cheques</label>
+                      <input class="form-control" type="number" id="cheque"  name="cheque" oninput="calcularCheque(this);" value="0" disabled>
+                      <label>Importe a pagar con cheques</label>
                     </div>
                   </div>
                   <div class="col-md-4 mb-4">
@@ -95,6 +95,18 @@ var ps = new PerfectScrollbar(sideNavScrollbar);
   $(document).ready(function() {
     $('.mdb-select').materialSelect();
   });
+
+  function calcularCheque(ele) {
+  var importe2 = document.getElementById("importe").value;
+  let efectivo = document.getElementById('efectivo');
+  efectivo.addEventListener("keyup", function(){
+  var efectivo = this.value;
+  var cheque = importe2-efectivo;
+  var cheque2 = cheque.toFixed(2);
+  document.getElementById('cheque').value = cheque2;
+  });
+}
+
 
 
 </script>
